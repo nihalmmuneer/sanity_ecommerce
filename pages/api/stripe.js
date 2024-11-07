d8925d4d3e728072a15d6c3b29b5320f2ffb5ca3
nihal <nihalmmuneer@gmail.com>
2024-11-08 00:28:30 +0530

    minor fixes

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
           };
         }),
         success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/canceled`,
+        cancel_url: `${req.headers.origin}/`,
       };
       const session = await stripe.checkout.sessions.create(params);
       //   res.redirect(303, session.url);
