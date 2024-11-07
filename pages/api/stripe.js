5d441e4726673e89d6bc79832102267280af0413
nihal <nihalmmuneer@gmail.com>
2024-11-07 22:38:52 +0530

    fixes

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -2,8 +2,6 @@ const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    req.body.map((item) => {
-    });
     try {
       // Create Checkout Sessions from body params.
       const params = {
